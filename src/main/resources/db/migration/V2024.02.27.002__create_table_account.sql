be7fe025313e924f3c448c6bbdeeb296f8074bdf
eriksoaress <erikbtu2020@gmail.com>
2024-04-07 21:39:17 -0300

    bd group

--- src/main/resources/db/migration/V2024.02.27.002__create_table_account.sql
+++ src/main/resources/db/migration/V2024.02.27.002__create_table_account.sql
@@ -1,8 +1,8 @@
-CREATE TABLE account
+CREATE TABLE group
 (
-    id_account character varying(36) NOT NULL,
+    id_group character varying(36) NOT NULL,
     tx_name character varying(256) NOT NULL,
-    tx_email character varying(256) NOT NULL,
-    tx_hash character varying(256) NOT NULL,
-    CONSTRAINT account_pkey PRIMARY KEY (id_account)
+    tx_description character varying(256) NOT NULL,
+    tx_qtd_members character INTEGER NOT NULL,
+    CONSTRAINT pk_group PRIMARY KEY (id_group)
 );
