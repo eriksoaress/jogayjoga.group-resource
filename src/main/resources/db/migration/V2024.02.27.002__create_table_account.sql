4d8b960fa6961c9bbfbc45438161f074161cc1ae
eriksoaress <erikbtu2020@gmail.com>
2024-04-08 13:57:32 -0300

    problema com nome tabela e schema

--- src/main/resources/db/migration/V2024.02.27.002__create_table_account.sql
+++ src/main/resources/db/migration/V2024.02.27.002__create_table_account.sql
@@ -1,8 +1,8 @@
-CREATE TABLE group
+CREATE TABLE tb_group
 (
     id_group character varying(36) NOT NULL,
     tx_name character varying(256) NOT NULL,
     tx_description character varying(256) NOT NULL,
-    tx_qtd_members character INTEGER NOT NULL,
+    tx_qtd_members INTEGER NOT NULL,
     CONSTRAINT pk_group PRIMARY KEY (id_group)
 );
